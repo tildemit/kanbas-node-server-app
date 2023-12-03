4be31559e6d0a6e1e08d4028f1906fc92ad9699a
Amit Chandra <xenopixelz@gmail.com>
2023-12-03 16:41:58 -0500

    A6 commit

--- app.js
+++ app.js
@@ -1,12 +1,38 @@
 import express from "express";
+import session from "express-session";
 import "dotenv/config";
 import Lab5 from "./lab5.js";
 import CourseRoutes from "./courses/routes.js";
 import ModuleRoutes from "./modules/routes.js";
 import cors from "cors";
+import mongoose from "mongoose";
+import UserRoutes from "./users/routes.js";
+const CONNECTION_STRING = process.env.DB_CONNECTION_STRING || 'mongodb://127.0.0.1:27017/kanbas';
+mongoose.connect(CONNECTION_STRING);
 const app = express();
+app.use(
+    cors({
+      credentials: true,
+      origin: process.env.FRONTEND_URL,
+    })
+   );
+   const sessionOptions = {
+    secret: "any string",
+    resave: false,
+    saveUninitialized: false,
+  };
+  if (process.env.NODE_ENV !== "development") {
+    sessionOptions.proxy = true;
+    sessionOptions.cookie = {
+      sameSite: "none",
+      secure: true,
+    };
+  }
+  app.use(session(sessionOptions));
+  
+  
 app.use(express.json());
-app.use(cors());
+UserRoutes(app);
 ModuleRoutes(app);
 CourseRoutes(app);
 Lab5(app);
